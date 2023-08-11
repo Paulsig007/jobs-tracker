6772c4b6df83dc03e54b1fe1a2c8b4e8c1684b56
Paulsig007 <131915869+Paulsig007@users.noreply.github.com>
2023-08-11 17:19:58 -0600

    save files properly to push properly

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [data, setData] = useState(null);
+
+  useEffect(() => {
+    fetch("/api")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
